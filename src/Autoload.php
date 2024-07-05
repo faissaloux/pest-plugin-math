97d58407bcd837cdfe6d947b2407a89cac30155e
faissaloux <fwahabali@gmail.com>
2024-07-05 16:02:24 +0100

    add phpstan to static workflow

--- src/Autoload.php
+++ src/Autoload.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Faissaloux\PestMath;
 
-use Pest\Expectation;
+use Pest\Mixins\Expectation;
 
 expect()->extend('toBeDivisibleBy', function (int $divisor): Expectation {
     return expect($this->value % $divisor === 0)->toBeTrue();
